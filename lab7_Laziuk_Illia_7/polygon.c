a5148926cbafe4ae4b4938fd4962eca6b32a55c9
illialaz <illia.laziuk2@gmail.com>
2019-12-03 13:11:52 +0300

    Modify polygon.c

--- lab7_Laziuk_Illia_7/polygon.c
+++ lab7_Laziuk_Illia_7/polygon.c
@@ -15,6 +15,7 @@ struct point{
 };
 
 struct polygon {
+  int num_of_points;
   struct point* point;
   int quarter;
 };
@@ -26,12 +27,12 @@ int compare (const void* pa, const void* pb){
   return quarter_a - quarter_b;
 }
 
-void fullfill(struct polygon* polygon, int num_of_polygons, int num_of_points) {
+void fullfill(struct polygon* polygon, int num_of_polygons) {
   for(int i = 0; i < num_of_polygons; i++) {
 
     polygon[i].quarter = 0;
 
-    for(int j = 0; j < num_of_points; j++) {
+    for(int j = 0; j < polygon[i].num_of_points; j++) {
       polygon[i].point[j].x = (double)(rand()%9) + 0.123746521 * (double)(rand()%10) - (double)(rand()%10) + 0.0578291 * (double)(rand()%10) - 0.1048310387 *(double)(rand()%10);
       polygon[i].point[j].y = (double)(rand()%9) + 0.123746521 * (double)(rand()%10) - (double)(rand()%10) + 0.0578291 * (double)(rand()%10) - 0.1048310387 *(double)(rand()%10);
       // scanf("%lf%lf", polygon[i].point[j].x, polygon[i].point[j].y);
@@ -39,9 +40,9 @@ void fullfill(struct polygon* polygon, int num_of_polygons, int num_of_points) {
   }
 }
 
-void in_which_quarter(struct polygon* polygon, int num_of_polygons, int num_of_points) {
+void in_which_quarter(struct polygon* polygon, int num_of_polygons) {
 for (int i = 0; i < num_of_polygons; i++) {
-   for (int j = 0; j < num_of_points; j++) {
+   for (int j = 0; j < polygon[i].num_of_points; j++) {
       if(polygon[i].point[j].x >= 0 && polygon[i].point[j].y >= 0) polygon[i].quarter += 1; // the point is in the 1 quarter
       else if (polygon[i].point[j].x < 0 && polygon[i].point[j].y > 0) polygon[i].quarter += 10;// the point is in the 2 quarter
       else if (polygon[i].point[j].x < 0 && polygon[i].point[j].y < 0) polygon[i].quarter += 100;// the point is in the 3 quater
@@ -50,17 +51,17 @@ for (int i = 0; i < num_of_polygons; i++) {
  }
 
   for (int i = 0; i < num_of_polygons; i++){
-    if(polygon[i].quarter == num_of_points) polygon[i].quarter = 1;
-    else if(polygon[i].quarter == num_of_points * 10) polygon[i].quarter = 2;
-    else if(polygon[i].quarter == num_of_points * 100) polygon[i].quarter = 3;
-    else if(polygon[i].quarter == num_of_points * 1000) polygon[i].quarter = 4;
+    if(polygon[i].quarter == polygon[i].num_of_points) polygon[i].quarter = 1;
+    else if(polygon[i].quarter == polygon[i].num_of_points * 10) polygon[i].quarter = 2;
+    else if(polygon[i].quarter == polygon[i].num_of_points * 100) polygon[i].quarter = 3;
+    else if(polygon[i].quarter == polygon[i].num_of_points * 1000) polygon[i].quarter = 4;
     else polygon[i].quarter = 5;
   }
 }
 
-void print_polygons(struct polygon* polygon, int num_of_polygons, int num_of_points) {
+void print_polygons(struct polygon* polygon, int num_of_polygons) {
   for(int i = 0; i < num_of_polygons; i++) {
-    for(int j = 0; j < num_of_points; j++) {
+    for(int j = 0; j < polygon[i].num_of_points; j++) {
       printf("(%.2lf, %.2lf) %d  ", polygon[i].point[j].x, polygon[i].point[j].y, polygon[i].quarter);
     }
     printf("\n");
@@ -68,25 +69,24 @@ void print_polygons(struct polygon* polygon, int num_of_polygons, int num_of_poi
 }
 
 int main(void) {
-  int num_of_points;
   int num_of_polygons;
-  printf("Enter number of polygons and number of points\n");
+  printf("Enter number of polygons\n");
   scanf("%d", &num_of_polygons);
-  scanf("%d", &num_of_points);
   struct polygon* polygon = (struct polygon*)malloc(num_of_polygons * sizeof(struct polygon));
   for (int i = 0; i < num_of_polygons; i++) {
-    polygon[i].point = (struct point*) malloc (num_of_points * sizeof(struct point));
+    polygon[i].num_of_points = (rand()%4 + 3);
+    polygon[i].point = (struct point*) malloc (polygon[i].num_of_points * sizeof(struct point));
   }
 
   srand(time(0));
   
-  fullfill(polygon, num_of_polygons, num_of_points);
+  fullfill(polygon, num_of_polygons);
 
-  in_which_quarter(polygon, num_of_polygons, num_of_points);
+  in_which_quarter(polygon, num_of_polygons);
 
   qsort(polygon, num_of_polygons, sizeof(struct polygon), compare);
 
-  print_polygons(polygon, num_of_polygons, num_of_points);
+  print_polygons(polygon, num_of_polygons);
 
   return 0;
 }
